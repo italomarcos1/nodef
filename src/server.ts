ca5f7b58b685f867753874bbc07209b314e0063e
Italo M <italomarcos0010@gmail.com>
2020-12-24 18:57:01 -0300

    a

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,9 @@
 import express from 'express';
 
-import routes from './routes';
-
 const server = express();
 
 server.get('/', (request, response) =>
   response.json({ message: 'batata doce' }),
 );
-
-server.use(express.json());
-
-server.use(routes);
 
 server.listen(3333, () => console.log('Rodando...'));
