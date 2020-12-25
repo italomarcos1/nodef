a04d460059bd559f1edc1bf1468c68f981912c85
Italo M <italomarcos0010@gmail.com>
2020-12-25 19:22:58 -0300

    aplicando DTOs no repositorio e model

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointment = appointmentsRepository.findByDate(parsedDate);
+
+  if (findAppointment)
+    return response
+      .status(400)
+      .json({ error: 'This appointment has been taken.' });
+
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
