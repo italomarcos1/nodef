02e93db38fa243724f8d9387ba81a332dee8fad8
Italo M <italomarcos0010@gmail.com>
2021-01-02 18:11:07 -0300

    criando o primeiro teste

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,19 @@
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create an appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointmentService.execute({
+      provider_id: '123',
+      date: new Date(),
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123');
+  });
+});
